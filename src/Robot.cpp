e20ce483671d770b9639f11cdb967d4cd779d8ab
William Kluge <wkluge17@moundsparkacademy.org>
2015-02-22 12:37:10 -0600

    2/22/2015 final build
    let's delete all these from this year next practice and make one that
    has the real code

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -3,51 +3,91 @@
 class Robot: public IterativeRobot
 {
 private:
-	CANTalon *Talon1;
-	CANTalon *Talon2;
-	CANTalon *Talon3;
-	CANTalon *Talon4;
-	RobotDrive *Drive;
-	Joystick *Leftstick;
-	Joystick *Rightstick;
+	CANTalon *TalonSRX1;
+	CANTalon *TalonSRX2;
+	CANTalon *TalonSRX3;
+	CANTalon *TalonSRX4;
+
+	RobotDrive *DriveSystem;
+
+	Joystick *LeftStick;
+	float LeftStickInput = 0.0;
+	Joystick *RightStick;
+	float RightStickInput = 0.0;
+	Joystick *XBoxPlayer1;
+
+	Talon *LiftMotor;
+
+	DoubleSolenoid *ArmSolenoid;
+
+	//Servo *servocam;
 
 	void RobotInit()
 	{
-		Talon1 = new CANTalon(1);
-		Talon2 = new CANTalon(2);
-		Talon3 = new CANTalon(3);
-		Talon4 = new CANTalon(4);
-		Drive = new RobotDrive(Talon1, Talon2, Talon3, Talon4);
-		Drive->SetSafetyEnabled(false);
-		Leftstick = new Joystick(0);
-		Rightstick = new Joystick(1);
-	}
-	void debug()
+		TalonSRX1 = new CANTalon(1);
+		TalonSRX2 = new CANTalon(2);
+		TalonSRX3 = new CANTalon(3);
+		TalonSRX4 = new CANTalon(4);
+
+		DriveSystem = new RobotDrive(TalonSRX1, TalonSRX2, TalonSRX3, TalonSRX4);
+
+		LeftStick = new Joystick(0);
+		RightStick = new Joystick(1);
+		XBoxPlayer1 = new Joystick(2);
+
+		LiftMotor = new Talon(0);
+
+		ArmSolenoid = new DoubleSolenoid(5, 7, 6);
+
+		//servocam = new Servo(0);
+	} //End RobotInit
 
 	void AutonomousInit()
 	{
 
-	}
+	} //End AutonomousInit
 
 	void AutonomousPeriodic()
 	{
 
-	}
-
+	} //End AutonomousPeriodic
 	void TeleopInit()
 	{
 
-	}
+	} //End TeleopInit
 
 	void TeleopPeriodic()
 	{
-		Drive->MecanumDrive_Cartesian(Leftstick->GetX(), Leftstick->GetY(), Rightstick->GetX());
-	}
+		DriveSystem->SetSafetyEnabled(false);
 
-	void TestPeriodic()
-	{
+		LeftStickInput = LeftStick->GetY();
+		RightStickInput = RightStick->GetY();
 
-	}
-};
+		LeftStickInput = LeftStickInput * -1;
+		RightStickInput = RightStickInput * -1;
+
+		if (RightStick->GetRawButton(1) == true)
+		{
+			LeftStickInput = RightStick->GetY();
+		}
+		if (LeftStick->GetRawButton(1) == true)
+		{
+			LeftStickInput = LeftStickInput / 2;
+			RightStickInput = RightStickInput / 2;
+		}
+
+		DriveSystem->TankDrive(LeftStickInput, RightStickInput, false); //KAWAII :3
+
+		if(XBoxPlayer1->GetRawButton(5) == true) LiftMotor->Set(-1);
+		if(XBoxPlayer1->GetRawButton(6) == true) LiftMotor->Set(1);
+		if(XBoxPlayer1->GetRawButton(5) != true and XBoxPlayer1->GetRawButton(6) != true) LiftMotor->Set(0.0);
+
+		if (XBoxPlayer1->GetRawButton(1) == true) ArmSolenoid->Set(DoubleSolenoid::Value::kForward);
+		if (XBoxPlayer1->GetRawButton(2) == true) ArmSolenoid->Set(DoubleSolenoid::Value::kReverse);
+		if (XBoxPlayer1->GetRawButton(1) != true and XBoxPlayer1->GetRawButton(2) != true) ArmSolenoid->Set(DoubleSolenoid::Value::kOff);
+
+		//servocam->SetAngle(RightStick->GetRawAxis(3));*/
+	}//End TeleopPeriodic
+}; //End Robot Class
 
 START_ROBOT_CLASS(Robot);
